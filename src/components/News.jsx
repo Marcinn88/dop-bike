938046d12e7f242cac3f977606ffd9aa814280e9
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2024-01-21 18:07:59 +0100

    work

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,54 +1,134 @@
 import React, { useState } from 'react';
 import styles from './News.module.css';
 import { nanoid } from 'nanoid';
+import leftImage from '../images/left-img.jpg';
+import rightImage from '../images/right-img.jpg';
+import { getDay, getMonth, getDefYear } from '../services/DateFunctions';
 
 export const News = () => {
   const data = [
     {
       _id: '1',
       title: 'Tytul Newsa',
-      date: '15.01.2024',
+      date: {
+        day: '21',
+        month: '01',
+        year: '2024',
+      },
       photo_position: 'left',
       photo: 'maciek3.jpg',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error illum mollitia beatae aliquam temporibus facilis suscipit, tempora, veniam optio, non dicta eveniet qui dolorem amet est debitis omnis ex expedita? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam exercitationem fuga mollitia adipisci saepe cupiditate quidem quam ex doloremque expedita dolorum assumenda facilis, sed aperiam maiores? Sapiente suscipit nam voluptate',
+      text1:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error illum mollitia beatae aliquam temporibus facilis suscipit, tempora, veniam optio, non dicta eveniet qui dolorem amet est debitis omnis ex expedita? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam exercitationem fuga mollitia adipisci saepe cupiditate quidem quam ex doloremque expedita dolorum assumenda facilis, sed aperiam maiores? Sapiente suscipit nam voluptate',
+      text2:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error illum mollitia beatae aliquam temporibus facilis suscipit, tempora, veniam optio, non dicta eveniet qui dolorem amet est debitis omnis ex expedita? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam exercitationem fuga mollitia adipisci saepe cupiditate quidem quam ex doloremque expedita dolorum assumenda facilis, sed aperiam maiores? Sapiente suscipit nam voluptate',
+      text3:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error illum mollitia beatae aliquam temporibus facilis suscipit, tempora, veniam optio, non dicta eveniet qui dolorem amet est debitis omnis ex expedita? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam exercitationem fuga mollitia adipisci saepe cupiditate quidem quam ex doloremque expedita dolorum assumenda facilis, sed aperiam maiores? Sapiente suscipit nam voluptate',
     },
     {
       _id: '2',
       title: 'Do czego służy drugie koło?',
-      date: '15.01.2024',
+      date: {
+        day: '20',
+        month: '01',
+        year: '2024',
+      },
       photo_position: 'right',
       photo: 'maciek2.jpg',
-      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione quasi dicta facere dolorem. Non laudantium expedita maiores in libero voluptate impedit nobis, veritatis eum enim quo illo reprehenderit, quidem laboriosam',
+      text1:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione quasi dicta facere dolorem. Non laudantium expedita maiores in libero voluptate impedit nobis, veritatis eum enim quo illo reprehenderit, quidem laboriosam',
+      text2: '',
+      text3: '',
     },
     {
       _id: '3',
       title: 'W00ooow, co się odstuntowało!',
-      date: '15.01.2024',
+      date: {
+        day: '15',
+        month: '12',
+        year: '2023',
+      },
       photo_position: 'left',
       photo: 'maciek.jpg',
-      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione quasi dicta facere dolorem. Non laudantium expedita maiores in libero voluptate impedit nobis, veritatis eum enim quo illo reprehenderit, quidem laboriosam',
+      text1:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione quasi dicta facere dolorem. Non laudantium expedita maiores in libero voluptate impedit nobis, veritatis eum enim quo illo reprehenderit, quidem laboriosam',
+      text2:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione quasi dicta facere dolorem. Non laudantium expedita maiores in libero voluptate impedit nobis, veritatis eum enim quo illo reprehenderit, quidem laboriosam',
+      text3:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione quasi dicta facere dolorem. Non laudantium expedita maiores in libero voluptate impedit nobis, veritatis eum enim quo illo reprehenderit, quidem laboriosam',
     },
     {
       _id: '4',
       title: 'AAAaaale duzo dymu!!!!',
-      date: '25.01.2024',
+      date: {
+        day: '01',
+        month: '10',
+        year: '2023',
+      },
       photo_position: 'right',
       photo: 'maciek4.jpg',
-      text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione quasi dicta facere dolorem. Non laudantium expedita maiores in libero voluptate impedit nobis, veritatis eum enim quo illo reprehenderit, quidem laboriosam',
+      text1:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione quasi dicta facere dolorem. Non laudantium expedita maiores in libero voluptate impedit nobis, veritatis eum enim quo illo reprehenderit, quidem laboriosam',
+      text2:
+        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione quasi dicta facere dolorem. Non laudantium expedita maiores in libero voluptate impedit nobis, veritatis eum enim quo illo reprehenderit, quidem laboriosam',
+      text3: '',
     },
   ];
   const [modal, setModal] = useState(false);
+  const [post, setPost] = useState({});
+  const [count, setCount] = useState(true);
 
   const openModal = () => {
+    setPost({
+      _id: nanoid(),
+      title: '',
+      date: {
+        day: getDay(),
+        month: getMonth(),
+        year: getDefYear(),
+      },
+      photo_position: 'right',
+      photo: '',
+      text1: '',
+      text2: '',
+      text3: '',
+    });
     setModal(true);
+    setCount(true);
+    console.log(post);
   };
 
   const closeModal = () => {
     setModal(false);
   };
-  const [count, setCount] = useState(true);
+
   const handleSlider = e => setCount(!count);
 
+  const setUploadFile = e => {
+    console.log('uploaded');
+    setPost({ ...post, photo: e.target.value });
+  };
+
+  const sliderTypeLeft = () => {
+    setPost({ ...post, photo_position: 'left' });
+    console.log('left');
+  };
+  const sliderTypeRight = () => {
+    setPost({ ...post, photo_position: 'right' });
+    console.log('right');
+  };
+  const dateTrim = e => {
+    const selectedData = e.target.value.toString();
+    const day = selectedData.substr(8, 2);
+    const month = selectedData.substr(5, 2);
+    const year = selectedData.substr(0, 4);
+    setPost({ ...post, date: { day: day, month: month, year: year } });
+  };
+  const submitModal = e => {
+    e.preventDefault();
+    // dispatch(createTransaction(data));
+    console.log(post);
+    setModal(!modal);
+  };
   return (
     <>
       <div className={styles.newsAdminPanel}>
@@ -62,12 +142,14 @@ export const News = () => {
           <div className={styles.addNewsModal}>
             <h1 className={styles.addNewsModalTitle}>Dodaj nowy wpis!</h1>
 
-            <form onSubmit="" className={styles.modalFormWrapper}>
+            <form onSubmit={submitModal} className={styles.modalFormWrapper}>
               <input
                 className={styles.modalTitle}
                 type="text"
                 name="title"
+                maxLength="50"
                 placeholder="Wpisz tytuł"
+                onChange={e => setPost({ ...post, title: e.target.value })}
                 required
               ></input>
 
@@ -82,10 +164,8 @@ export const News = () => {
                     type="checkbox"
                     className={styles.checkbox}
                     name="slider"
-                    value={!count ? '+' : '-'}
-                    onChange={
-                      !count ? console.log('left') : console.log('right')
-                    }
+                    value={!count ? 'Right' : 'Left'}
+                    onChange={!count ? sliderTypeRight : sliderTypeLeft}
                     onClick={handleSlider}
                   />
                   <span className={styles.slider}></span>
@@ -95,33 +175,62 @@ export const News = () => {
                 ) : (
                   <span className={styles.redText}>Zdjęcie z prawej</span>
                 )}
+
+                {!count ? (
+                  <img
+                    className={styles.switchImage}
+                    src={leftImage}
+                    alt="wzor formularza"
+                  />
+                ) : (
+                  <img
+                    className={styles.switchImage}
+                    src={rightImage}
+                    alt="wzor formularza"
+                  />
+                )}
               </div>
 
-              <button className={styles.modalNewsBtn}>Załącz zdjęcie</button>
-              <input
-                className={styles.modalData}
-                type="date"
-                name="date"
-              ></input>
+              <div className={styles.modalSmallWrapper}>
+                <label className={styles.modalFile} id="upload-label">
+                  <input
+                    className={styles.modalInputFile}
+                    type="file"
+                    id="upload-file"
+                    onChange={setUploadFile}
+                  ></input>
+                  <em>Załącz zdjęcie</em>
+                </label>
+                <input
+                  className={styles.modalData}
+                  type="date"
+                  name="date"
+                  onChange={dateTrim}
+                ></input>
+              </div>
               <textarea
                 type="textarea"
                 maxLength="1000"
                 name="paragraf"
-                placeholder="Paragraf1"
+                placeholder="Wpisz zawartość pierwszego Paragrafu"
+                onChange={e => setPost({ ...post, text1: e.target.value })}
+                className={styles.modalParagraf}
+                required
+              ></textarea>
+              <textarea
+                type="textarea"
+                maxLength="1000"
+                name="paragraf"
+                placeholder="Wpisz zawartość drugiego Paragrafu (opcjonalnie)"
+                onChange={e => setPost({ ...post, text2: e.target.value })}
                 className={styles.modalParagraf}
               ></textarea>
               <textarea
                 type="textarea"
                 maxLength="1000"
                 name="paragraf"
-                placeholder="Paragraf1"
-                className={styles.modalParagraf}
-              ></textarea>
-              <textarea
-                type="textarea"
-                maxLength="1000"
-                name="paragraf"
-                placeholder="Paragraf1"
+                placeholder="Wpisz zawartość trzeciego Paragrafu (opcjonalnie)"
+                onChange={e => setPost({ ...post, text3: e.target.value })}
                 className={styles.modalParagraf}
               ></textarea>
               <div className={styles.addNewsModalBtns}>
@@ -140,31 +249,52 @@ export const News = () => {
           <h1 className={styles.newsTitle}>Aktualności</h1>
         </div>
 
-        {data.map(({ _id, title, date, photo_position, photo, text }) => {
-          return (
-            <div className={styles.newsElement} id={_id ?? nanoid()}>
-              <p className={styles.newsSubTitle}>{title}</p>
-              <p className={styles.newsDate}>{date}</p>
-              <div className={styles.newsContainer}>
-                {photo_position === 'left' ? (
-                  <>
-                    <div className={styles.newsImg}>
-                      <img src={require(`../images/${photo}`)} alt="bike" />
-                    </div>
-                    <div className={styles.newsText}>{text}</div>
-                  </>
-                ) : (
-                  <>
-                    <div className={styles.newsText}>{text}</div>
-                    <div className={styles.newsImg}>
-                      <img src={require(`../images/${photo}`)} alt="bike" />
-                    </div>
-                  </>
-                )}
+        {data.map(
+          ({
+            _id,
+            title,
+            date,
+            photo_position,
+            photo,
+            text1,
+            text2,
+            text3,
+          }) => {
+            return (
+              <div className={styles.newsElement} id={_id ?? nanoid()}>
+                <p className={styles.newsSubTitle}>{title}</p>
+                <p className={styles.newsDate}>
+                  {date.day}.{date.month}.{date.year}
+                </p>
+                <div className={styles.newsContainer}>
+                  {photo_position === 'left' ? (
+                    <>
+                      <div className={styles.newsImg}>
+                        <img src={require(`../images/${photo}`)} alt="bike" />
+                      </div>
+                      <div className={styles.newsText}>
+                        <p>{text1}</p>
+                        <p>{text2}</p>
+                        <p>{text3}</p>
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <div className={styles.newsText}>
+                        <p>{text1}</p>
+                        <p>{text2}</p>
+                        <p>{text3}</p>
+                      </div>
+                      <div className={styles.newsImg}>
+                        <img src={require(`../images/${photo}`)} alt="bike" />
+                      </div>
+                    </>
+                  )}
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          }
+        )}
       </div>
     </>
   );

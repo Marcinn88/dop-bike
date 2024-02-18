ad72c6a07eef11cd69e04db14a917c23db8104b5
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2024-02-18 18:36:33 +0100

    admin bugfix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,23 @@ import { News } from './News';
 import { Footer } from './Footer';
 
 export const App = () => {
-  const token = localStorage.getItem('token');
-  const parsedToken = JSON.parse(token).token;
-  // console.log(parsedToken.token);
+  const tokenChecker = () => {
+    try {
+      const token = localStorage.getItem('token');
+      const parsedToken = JSON.parse(token).token;
+      return parsedToken;
+    } catch (error) {
+      localStorage.setItem('token', JSON.stringify({ token: '' }));
+      const token = localStorage.getItem('token');
+      const parsedToken = JSON.parse(token).token;
+      return parsedToken;
+    }
+  };
 
   return (
     <>
       <Header />
-      <News token={parsedToken} />
+      <News token={tokenChecker()} />
       <Footer />
     </>
   );

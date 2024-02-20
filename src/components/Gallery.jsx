ef7c7f93eb3468db4809d09dc15a309262098405
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2024-02-20 19:09:42 +0100

    gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,6 +3,9 @@ import { Nav } from './Nav';
 import image1 from '../images/maciek.jpg';
 import image2 from '../images/maciek2.jpg';
 import image3 from '../images/maciek3.jpg';
+import image4 from '../images/maciek4.jpg';
+import image5 from '../images/motor.jpg';
+import image6 from '../images/ai-bike2.jpg';
 
 export const Gallery = ({ token }) => {
   return (
@@ -31,18 +34,18 @@ export const Gallery = ({ token }) => {
               <button className={styles.galleryBtn}>Otwórz</button>
             </li>
             <li className={styles.galleryElement}>
-              <p className={styles.gallerySubTitle}>Wadowice - 2024</p>
-              <img className={styles.gallerySubTitle} src={image1} alt="bike" />
+              <p className={styles.gallerySubTitle}>Koszelówka - 2024</p>
+              <img className={styles.gallerySubTitle} src={image4} alt="bike" />
               <button className={styles.galleryBtn}>Otwórz</button>
             </li>
             <li className={styles.galleryElement}>
-              <p className={styles.gallerySubTitle}>Ciachcin - 2024</p>
-              <img className={styles.gallerySubTitle} src={image2} alt="bike" />
+              <p className={styles.gallerySubTitle}>Parking</p>
+              <img className={styles.gallerySubTitle} src={image5} alt="bike" />
               <button className={styles.galleryBtn}>Otwórz</button>
             </li>
             <li className={styles.galleryElement}>
-              <p className={styles.gallerySubTitle}>Sosnowiec - 2024</p>
-              <img className={styles.gallerySubTitle} src={image3} alt="bike" />
+              <p className={styles.gallerySubTitle}>Tajne zdjęcia</p>
+              <img className={styles.gallerySubTitle} src={image6} alt="bike" />
               <button className={styles.galleryBtn}>Otwórz</button>
             </li>
           </ul>

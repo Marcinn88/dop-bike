c51727a135d9ff9722937c8a9b6c979000e2a2f6
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2024-02-20 21:08:58 +0100

    gallery work

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,5 +1,7 @@
 import styles from './Gallery.module.css';
 import { Nav } from './Nav';
+import { useState } from 'react';
+
 import image1 from '../images/maciek.jpg';
 import image2 from '../images/maciek2.jpg';
 import image3 from '../images/maciek3.jpg';
@@ -8,9 +10,39 @@ import image5 from '../images/motor.jpg';
 import image6 from '../images/ai-bike2.jpg';
 
 export const Gallery = ({ token }) => {
+  const [gallery, setGallery] = useState(false);
+
+  const openGallery = () => {
+    setGallery(true);
+  };
+
+  const closeGallery = () => {
+    setGallery(false);
+  };
+
   return (
     <>
       <div className={styles.galleryWrapper}>
+        {gallery && (
+          <>
+            <div
+              className={styles.galleryShadowBox}
+              onClick={() => {
+                closeGallery();
+              }}
+            ></div>
+            <div className={styles.galleryMainModal}>
+              <button
+                onClick={() => {
+                  closeGallery();
+                }}
+              >
+                Zamknij
+              </button>
+            </div>
+          </>
+        )}
+
         <Nav selected={'gallery'} token={token} />
         <p className={styles.galleryTitle}>Galeria!</p>
         <p className={styles.galleryText}>
@@ -18,7 +50,12 @@ export const Gallery = ({ token }) => {
         </p>
         <div className={styles.galleryMain}>
           <ul className={styles.galleryList}>
-            <li className={styles.galleryElement}>
+            <li
+              className={styles.galleryElement}
+              onClick={() => {
+                openGallery();
+              }}
+            >
               <img className={styles.gallerySubTitle} src={image1} alt="bike" />
               <p className={styles.gallerySubTitle}>Wadowice - 2024</p>
               <p className={styles.gallerySubText}>
@@ -27,7 +64,12 @@ export const Gallery = ({ token }) => {
               </p>
               <button className={styles.galleryBtn}>Otwórz</button>
             </li>
-            <li className={styles.galleryElement}>
+            <li
+              className={styles.galleryElement}
+              onClick={() => {
+                openGallery();
+              }}
+            >
               <img className={styles.gallerySubTitle} src={image2} alt="bike" />
               <p className={styles.gallerySubTitle}>Ciachcin - 2024</p>
               <p className={styles.gallerySubText}>
@@ -35,7 +77,12 @@ export const Gallery = ({ token }) => {
               </p>
               <button className={styles.galleryBtn}>Otwórz</button>
             </li>
-            <li className={styles.galleryElement}>
+            <li
+              className={styles.galleryElement}
+              onClick={() => {
+                openGallery();
+              }}
+            >
               <img className={styles.gallerySubTitle} src={image3} alt="bike" />
               <p className={styles.gallerySubTitle}>Sosnowiec - 2024</p>
               <p className={styles.gallerySubText}>
@@ -44,7 +91,12 @@ export const Gallery = ({ token }) => {
               </p>
               <button className={styles.galleryBtn}>Otwórz</button>
             </li>
-            <li className={styles.galleryElement}>
+            <li
+              className={styles.galleryElement}
+              onClick={() => {
+                openGallery();
+              }}
+            >
               <img className={styles.gallerySubTitle} src={image4} alt="bike" />
               <p className={styles.gallerySubTitle}>Koszelówka - 2024</p>
               <p className={styles.gallerySubText}>
@@ -52,7 +104,12 @@ export const Gallery = ({ token }) => {
               </p>
               <button className={styles.galleryBtn}>Otwórz</button>
             </li>
-            <li className={styles.galleryElement}>
+            <li
+              className={styles.galleryElement}
+              onClick={() => {
+                openGallery();
+              }}
+            >
               <img className={styles.gallerySubTitle} src={image5} alt="bike" />
               <p className={styles.gallerySubTitle}>Parking</p>
               <p className={styles.gallerySubText}>
@@ -60,7 +117,12 @@ export const Gallery = ({ token }) => {
               </p>
               <button className={styles.galleryBtn}>Otwórz</button>
             </li>
-            <li className={styles.galleryElement}>
+            <li
+              className={styles.galleryElement}
+              onClick={() => {
+                openGallery();
+              }}
+            >
               <img className={styles.gallerySubTitle} src={image6} alt="bike" />
               <p className={styles.gallerySubTitle}>Tajne zdjęcia</p>
               <p className={styles.gallerySubText}>Ściśle tajne i poufne.</p>
